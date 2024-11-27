93cfaa3f558a0020000b4515576f1ce9f59f8833
Darloscode <carfgonz@espol.edu.ec>
2024-11-27 10:57:49 -0500

    Se agregó nuevas funcionalidades y eventos

--- src/components/ControlWeather.tsx
+++ src/components/ControlWeather.tsx
@@ -1,4 +1,7 @@
-{/* Componentes MUI */}
+{/* Hooks */ }
+import { useState, useRef } from 'react';
+
+{/* Componentes MUI */ }
 
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
@@ -6,22 +9,40 @@ import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
 
+{/* Interfaz SelectChangeEvent */ }
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 export default function ControlWeather() {
+    {/* Constante de referencia a un elemento HTML */ }
+    const descriptionRef = useRef<HTMLDivElement>(null);
 
-    {/* Arreglo de objetos */}
+    {/* Variable de estado y función de actualización */ }
+    let [selected, setSelected] = useState(-1)
+
+    {/* Arreglo de objetos */ }
     let items = [
-        {"name":"Precipitación", "description":"Cantidad de agua que cae sobre una superficie en un período específico."}, 
-        {"name": "Humedad", "description":"Cantidad de vapor de agua presente en el aire, generalmente expresada como un porcentaje."}, 
-        {"name":"Nubosidad", "description":"Grado de cobertura del cielo por nubes, afectando la visibilidad y la cantidad de luz solar recibida."}
+        { "name": "Precipitación", "description": "Cantidad de agua que cae sobre una superficie en un período específico." },
+        { "name": "Humedad", "description": "Cantidad de vapor de agua presente en el aire, generalmente expresada como un porcentaje." },
+        { "name": "Nubosidad", "description": "Grado de cobertura del cielo por nubes, afectando la visibilidad y la cantidad de luz solar recibida." }
     ]
 
-    {/* Arreglo de elementos JSX */}
-    let options = items.map( (item, key) => <MenuItem key={key} value={key}>{item["name"]}</MenuItem> )
-       
-    {/* JSX */}
+    {/* Arreglo de elementos JSX */ }
+    let options = items.map((item, key) => <MenuItem key={key} value={key}>{item["name"]}</MenuItem>)
+
+    {/* Manejador de eventos */ }
+    const handleChange = (event: SelectChangeEvent) => {
+
+        let idx = parseInt(event.target.value)
+        /*alert(idx);*/
+        setSelected(idx);
+        {/* Modificación de la referencia descriptionRef */ }
+        if (descriptionRef.current !== null) {
+            descriptionRef.current.innerHTML = (idx >= 0) ? items[idx]["description"] : ""
+        }
+    };
+
+    {/* JSX */ }
     return (
         <Paper
             sx={{
@@ -36,7 +57,7 @@ export default function ControlWeather() {
             </Typography>
 
             <Box sx={{ minWidth: 120 }}>
-                   
+
                 <FormControl fullWidth>
                     <InputLabel id="simple-select-label">Variables</InputLabel>
                     <Select
@@ -44,6 +65,7 @@ export default function ControlWeather() {
                         id="simple-select"
                         label="Variables"
                         defaultValue='-1'
+                        onChange={handleChange}
                     >
                         <MenuItem key="-1" value="-1" disabled>Seleccione una variable</MenuItem>
 
@@ -54,6 +76,13 @@ export default function ControlWeather() {
 
             </Box>
 
+            {/* Use la variable de estado para renderizar del item seleccionado */}
+            {/*<Typography mt={2} component="p" color="text.secondary">
+             {
+                 (selected >= 0)?items[selected]["description"]:""
+             }
+             </Typography>*/}
+            <Typography ref={descriptionRef} mt={2} component="p" color="text.secondary" />
 
         </Paper>
 
